929515e4937b6f2de4dff9d5196e54424b54d25b
michealapalmer <michealapalmer@LAB989096B63A27.trent.trentu.ca>
2018-01-15 17:54:20 -0500

    master file for printing out a string to the screen

---
+++ src/secondClass.java
@@ -0,0 +1,12 @@
+
+public class secondClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String sl = "Hello World";
+		System.out.println("Give a string:");
+		System.out.println("\n String is " + sl);
+
+	}
+
+}
